f4c640d176150ed07f36b2d544adc09a4b6cfbe7
joseph uzuegbu <josephuzuegbu55@gmail.com>
2024-04-23 15:39:08 +0100

    fix the room name on mobile

--- client/src/pages/Chats.tsx
+++ client/src/pages/Chats.tsx
@@ -3,27 +3,66 @@ import MessageRoomCard from "./Home/components/MessageRoomCard";
 import { useSelector, useDispatch } from "react-redux";
 import { chatAppType } from "../sliceType";
 import { addRoomData } from "../slice";
+import { ChatDataType } from "../type/chatDataType";
+import { useEffect, useState } from "react";
+import { messageRoomType } from "../type/messageRoomType";
+import axios from "axios";
+import { useUser } from "../customHooks/useUser";
 
 export default function Chats() {
   const dispatch = useDispatch();
+  const [roomCredential, setroomCredential] = useState<messageRoomType[]>([]);
+  const [status, setStatus] = useState("idle");
 
-  const rooms = useSelector((state: chatAppType) => state.roomCredential);
+  const user = useUser();
 
-  const handleGetCreateRoom = (roomsCredential: any) => {
-    return dispatch(addRoomData(roomsCredential));
-  };
+  // const rooms = useSelector((state: chatAppType) => state.roomCredential);
 
-  if (socket) {
+  useEffect(() => {
+    const handleGetCreateRoom = (roomsCredential: any) => {
+      setroomCredential(roomsCredential);
+    };
     socket.on("getCreateRoom", handleGetCreateRoom);
+
+    return () => {
+      socket.off("getCreateRoom", handleGetCreateRoom);
+    };
+  }, []);
+  // const handleGetCreateRoom = (roomsCredential: any) => {
+  //   setroomCredential(roomsCredential);
+  // };
+
+  // if (socket) {
+  //   socket.on("getCreateRoom", handleGetCreateRoom);
+  // }
+
+  // if (socket) {
+  //   socket.off("getCreateRoom", handleGetCreateRoom);
+  // }
+
+  async function getRoomsDB() {
+    try {
+      const req = await axios({
+        method: "get",
+        url: `http://localhost:7000/room/${user.data.email}`,
+      });
+      // console.log(req.data);
+      setroomCredential(req.data);
+    } catch (error) {
+      setStatus("error");
+    }
   }
 
-  if (socket) {
-    socket.off("getCreateRoom", handleGetCreateRoom);
-  }
+  useEffect(() => {
+    getRoomsDB();
+    return () => {
+      getRoomsDB();
+    };
+  });
 
   return (
-    <div className="flex flex-col h-full space-y-4">
-      {rooms.map((item, index) => (
+    <div className="flex flex-col h-full space-y-4 p-2">
+      {roomCredential.map((item, index) => (
         <MessageRoomCard key={index} item={item} />
       ))}
     </div>
