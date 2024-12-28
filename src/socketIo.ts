1f7aef3fc83e9f6094d4e055392d20a880197840
josephDev123 <josephuzuegbu55@gmail.com>
2024-12-28 05:46:37 +0100

    production url for cors bug

--- src/socketIo.ts
+++ src/socketIo.ts
@@ -2,9 +2,9 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production"
-    ? import.meta.env.VITE_BASEURL
-    : import.meta.env.VITE_BASEURL;
+  // process.env.NODE_ENV === "production"
+  //   ? import.meta.env.VITE_BASEURL
+  import.meta.env.VITE_BASEURL;
 
 export const socket = io(URL!);
 
