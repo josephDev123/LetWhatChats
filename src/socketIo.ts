5747a3fd5c1232ae84ad0fa5b400c1341f7f1aa2
josephDev123 <josephuzuegbu55@gmail.com>
2024-12-28 06:04:39 +0100

    socket-clent  url for cors bug

--- src/socketIo.ts
+++ src/socketIo.ts
@@ -1,11 +1,5 @@
 import { io } from "socket.io-client";
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL =
-  // process.env.NODE_ENV === "production"
-  //   ? import.meta.env.VITE_BASEURL
-  import.meta.env.VITE_BASEURL;
+const URL = import.meta.env.VITE_BASEURL;
 
 export const socket = io(URL!);
-
-// export const socket = io("http://localhost:7000");
